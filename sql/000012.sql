6a00a182e22280d9931e4c26d7ff42e4b41a5f69
William Oliveira <william.oliveir4@gmail.com>
2022-02-22 19:40:12 -0300

    fix: fix unique_violation on complete_job and fail_job

---
+++ sql/000012.sql
@@ -0,0 +1,80 @@
+create or replace function :GRAPHILE_WORKER_SCHEMA.complete_job(worker_id text, job_id bigint) returns :GRAPHILE_WORKER_SCHEMA.jobs as $$
+declare
+v_row :GRAPHILE_WORKER_SCHEMA.jobs;
+declare
+v_next_job_id bigint;
+declare now timestamp with time zone := now();
+declare job_expiry interval := '04:00:00'::interval;
+begin
+delete from :GRAPHILE_WORKER_SCHEMA.jobs
+where id = job_id
+    returning * into v_row;
+
+if v_row.queue_name is not null then
+    select jobs.id into v_next_job_id
+    from "graphile_worker".jobs
+    where (jobs.locked_at is null or jobs.locked_at < (now - job_expiry))
+      and queue_name = v_row.queue_name
+      and front_of_the_queue = false
+      and attempts < max_attempts
+    order by priority asc, run_at asc, id asc
+    limit 1
+    for update
+    skip locked;
+
+    begin
+        update "graphile_worker".jobs
+        set front_of_the_queue = true
+        where id = v_next_job_id;
+    exception
+        when unique_violation then
+            null;
+    end;
+end if;
+
+return v_row;
+end;
+$$ language plpgsql;
+
+create or replace function :GRAPHILE_WORKER_SCHEMA.fail_job(worker_id text, job_id bigint, error_message text) returns :GRAPHILE_WORKER_SCHEMA.jobs as $$
+declare
+v_row :GRAPHILE_WORKER_SCHEMA.jobs;
+declare
+v_next_job_id bigint;
+declare now timestamp with time zone := now();
+declare job_expiry interval := '04:00:00'::interval;
+begin
+update :GRAPHILE_WORKER_SCHEMA.jobs
+set
+    last_error = error_message,
+    run_at = greatest(now(), run_at) + (exp(least(attempts, 10))::text || ' seconds')::interval,
+    locked_by = null,
+    locked_at = null
+where id = job_id and locked_by = worker_id
+    returning * into v_row;
+
+if v_row.queue_name is not null then
+    select jobs.id into v_next_job_id
+    from "graphile_worker".jobs
+    where (jobs.locked_at is null or jobs.locked_at < (now - job_expiry))
+      and queue_name = v_row.queue_name
+      and front_of_the_queue = false
+      and attempts < max_attempts
+    order by priority asc, run_at asc, id asc
+    limit 1
+    for update
+    skip locked;
+
+    begin
+        update "graphile_worker".jobs
+        set front_of_the_queue = true
+        where id = v_next_job_id;
+    exception
+        when unique_violation then
+            null;
+    end;
+end if;
+
+return v_row;
+end;
+$$ language plpgsql volatile strict;
